bda6d63958e085fe9e8e5d069d1740c8b46c7459
leobrowaldh <leobrowaldh@gmail.com>
2024-02-05 15:29:19 +0100

    UI Card List implemented

---
+++ CSLGaming.UI/Classes/ProductCard.cs
@@ -0,0 +1,8 @@
+﻿namespace CSLGaming.UI.Classes;
+
+public class ProductCard
+{
+    public string Image {  get; set; }
+    public string Price { get; set; }
+    public string Title { get; set; }
+}
