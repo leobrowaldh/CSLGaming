80b9c40901a869782969c161e21656a0e8c2193f
Steffebresse <143505834+Steffebresse@users.noreply.github.com>
2024-02-09 01:10:42 +0100

    Links ful

---
+++ CSLGaming.UI.Models/Link/LinkOption.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace CSLGaming.UI.Models.Link
+{
+    public class LinkOption
+    {
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsSelected {  get; set; }
+    }
+}
---
+++ CSLGaming.UI.Razor/Areas/MyFeature/Pages/Page1.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CSLGaming.UI.Razor.MyFeature.Pages
+{
+    public class Page1Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
