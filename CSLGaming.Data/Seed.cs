8754025c3b454eba12b960966e63d80e4c9c1838
leobrowaldh <leobrowaldh@gmail.com>
2024-02-26 21:18:59 +0100

    game images added in order and numbered following seed structure

--- CSLGaming.Data/Seed.cs
+++ CSLGaming.Data/Seed.cs
@@ -7,7 +7,7 @@ internal class Seed
     public List<string> categories = new List<string>() {"Windows", "Mac", "PS", "XBox", "WII"};
     public List<string> products = new List<string>() 
     { 
-        "Doom2", "Age of Empires", "Zelda", "World of warcraft", "Dungeon siege", "Blade", "Warcraft3", "Tekken", "Diablo2", "Diablo4",
+        "Doom2", "Age of Empires", "Zelda", "World of warcraft", "Dungeon siege", "GTA Vice City", "Warcraft3", "Tekken", "Diablo2", "Diablo4",
         "Sims2", "7 days to die", "Stronghold", "Dawn of war", "Half Life 2", "Mario Karts", "Pikmin", "Mortal Combat 4", "Need for spped 3", "Descent 2"
     };
     public List<int> ageRestrictions = new List<int>() { 3, 7, 12, 16, 18 };
