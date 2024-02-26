5203a4073c9843396565eadadd4338b1d4cba158
leobrowaldh <leobrowaldh@gmail.com>
2024-02-26 20:46:01 +0100

    started seed class

--- CSLGaming.Data/Entities/AgeRestriction.cs
+++ CSLGaming.Data/Entities/AgeRestriction.cs
@@ -4,6 +4,7 @@ namespace CSLGaming.Data;
 public class AgeRestriction : IEntity
 {
     public int Id { get; set; }
+    //3, 7, 12, 16, 18 are the possible age restrictions
     public int Age { get; set; }
     public enOptionType OptionType { get; set; }
     public List<Product>? Products { get; set; }
