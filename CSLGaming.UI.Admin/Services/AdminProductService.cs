d2b77f03c2fc48db0ac8c087e59e4b24e001126f
Stefan kureljusic <stefan.kureljusic@hotmail.com>
2024-03-07 12:04:34 +0100

    Updated UIAdmin product props and comoponents

--- CSLGaming.UI.Admin/Services/AdminProductService.cs
+++ CSLGaming.UI.Admin/Services/AdminProductService.cs
@@ -6,17 +6,64 @@ namespace CSLGaming.UI.Admin.Services
     public class AdminProductService
     {
         private readonly AdminProductHttpClient _httpClient;
+        private readonly AdminCategoryService _categoryService;
+        
 
         public List<ProductGetDTO> Products { get; set; }
 
-        public AdminProductService(AdminProductHttpClient httpClient)
+        public List<CategoryGetDTO> Categories { get; set; } = new List<CategoryGetDTO>();
+
+        public List<GenereGetDTO> Generes {  get; set; }
+
+        public List<AgeRestrictionGetDTO> AgeRestrictions { get; set; }
+
+        public AdminProductService(AdminProductHttpClient httpClient, AdminCategoryService categoryService)
         {
                 _httpClient = httpClient;
+            
+            _categoryService = categoryService;
+            Categories = _categoryService.Categories;
+        }
+
+        public async Task Addproduct(ProductPostDTO product)
+        {
+            
+
+     
+            await _httpClient.AddProduct(product);
+
+        }
+
+        public async Task UpdateConnections(int productId, int CatId,int GenereId)
+        {
+            await _httpClient.PostProductWithKopplingar(productId, CatId, GenereId);
         }
 
         public async Task GetAllProducts() => Products = await _httpClient.GetProducts();
 
         public async Task DeleteProd(int id) => await _httpClient.DeleteProductAdmin(id); 
+
+
+        
+        public async Task GetEtc()
+        {
+            try
+            {
+                Generes = await _httpClient.GetGen();
+
+                //AgeRestrictions = await _httpClient.GetAge();
+
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+
+        }
+
 
     }
 }
--- CSLGaming.UI.Http/Clients/AdminProductHttpClient.cs
+++ CSLGaming.UI.Http/Clients/AdminProductHttpClient.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSLGaming.API.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Json;
@@ -11,6 +12,9 @@ namespace CSLGaming.UI.Http.Clients
     {
         private readonly HttpClient _httpClient;
         string _baseAdress = "https://localhost:5500"; // Hämta basadressen som är densamma som api!
+
+        
+
 
         public AdminProductHttpClient(HttpClient httpClient)
         {
@@ -40,6 +44,60 @@ namespace CSLGaming.UI.Http.Clients
            
         }
 
+        public async Task AddProduct(ProductPostDTO product)
+        {
+            try
+            {
+                var jsonContent = new StringContent(JsonSerializer.Serialize(product), Encoding.UTF8, "application/json");
+
+                using HttpResponseMessage response = await _httpClient.PostAsync("/api/products", jsonContent);
+                response.EnsureSuccessStatusCode();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+
+        public async Task PostProductWithKopplingar(int ProductId, int CatId, int GenereId)
+        {
+            
+
+            ProductCategoryPostDTO ProductCategory = new()
+            {
+                ProductId = ProductId,
+                CategoryId = CatId
+            };
+
+            ProductGenerePostDTO ProductGenere = new() { GenereId = GenereId, ProductId = ProductId };
+
+            
+
+            try
+            {
+                
+                
+                var jsonContentC = new StringContent(JsonSerializer.Serialize(ProductCategory), Encoding.UTF8, "application/json");
+
+                using HttpResponseMessage responseC = await _httpClient.PostAsync("/api/categoryproducts", jsonContentC);
+                responseC.EnsureSuccessStatusCode();
+
+                var jsonContentG = new StringContent(JsonSerializer.Serialize(ProductGenere), Encoding.UTF8, "application/json");
+
+                using HttpResponseMessage responseG = await _httpClient.PostAsync("/api/genereproducts", jsonContentG);
+                responseG.EnsureSuccessStatusCode();
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
         public async Task DeleteProductAdmin(int id)
         {
             try
@@ -55,6 +113,49 @@ namespace CSLGaming.UI.Http.Clients
             }
 
         }   
+
+
+        public async Task <List<GenereGetDTO>> GetGen()
+        {
+            try
+            {
+                string geturl = "/api/Generes";
+
+                using HttpResponseMessage response = await _httpClient.GetAsync(geturl);
+                response.EnsureSuccessStatusCode();
+
+                var result = await response.Content.ReadFromJsonAsync<List<GenereGetDTO>>();
+
+                return result ?? [];
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+
+        public async Task<List<AgeRestrictionGetDTO>> GetAge()
+        {
+            try
+            {
+                string geturl = "/api/Generes";
+
+                using HttpResponseMessage response = await _httpClient.GetAsync(geturl);
+                response.EnsureSuccessStatusCode();
+
+                var result = await response.Content.ReadFromJsonAsync<List<AgeRestrictionGetDTO>>();
+
+                return result ?? [];
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        
 
     }
 }
